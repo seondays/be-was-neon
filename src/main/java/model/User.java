81548649fa5537b5a20a37ffafa3d25af00d71f0
seondays <agseon9@gmail.com>
2024-03-13 12:10:35 +0900

    feat : User 생성자 추가
    Map으로 된 구조를 가지고 User로 저장할 수 있도록 하기 위해 생성자 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,6 +1,14 @@
 package model;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
 public class User {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private String userId;
     private String password;
     private String name;
@@ -11,6 +19,24 @@ public class User {
         this.password = password;
         this.name = name;
         this.email = email;
+        logger.debug(this.toString());
+    }
+
+    public User(Map<String, String> info) {
+        this.userId = info.get("userId");
+        this.password = info.get("password");
+        this.name = nameDecoder(info.get("name"));
+        this.email = info.get("email");
+        logger.debug(this.toString());
+    }
+
+    private String nameDecoder(String name) {
+        try {
+            name = URLDecoder.decode(name,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return name;
     }
 
     public String getUserId() {

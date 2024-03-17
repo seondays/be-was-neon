a3d6f7b4e6e83ea33a761ba37a791f05845ffa5d
seondays <agseon9@gmail.com>
2024-03-18 02:41:15 +0900

    comment : 주석 스타일 변경
    javadoc 스타일로 주석 변경

--- src/main/java/utils/Path.java
+++ src/main/java/utils/Path.java
@@ -7,11 +7,11 @@ public class Path {
     public static final String BASE_PATH = "src/main/resources/static";
     public static final String DEFAULT_FILE = "/index.html";
 
-    /*
-     요청받은 리소스 정보를 가지고 유효한 주소를 만들어준다.
-     요청 리소스가 디렉토리라면 해당 디렉토리의 index.html 파일의 주소를,
-     파일이라면 해당 파일의 주소를 생성한 후에, 해당 주소가 유효한지 확인한다.
-     유효하지 않은 파일일 경우 예외를 발생시킨다.
+    /**
+     *      요청받은 리소스 정보를 가지고 유효한 주소를 만들어준다.
+     *      요청 리소스가 디렉토리라면 해당 디렉토리의 index.html 파일의 주소를,
+     *      파일이라면 해당 파일의 주소를 생성한 후에, 해당 주소가 유효한지 확인한다.
+     *      유효하지 않은 파일일 경우 예외를 발생시킨다.
      */
     public String buildURL(String resource) throws FileNotFoundException {
         StringBuffer result = new StringBuffer(BASE_PATH);
@@ -34,7 +34,7 @@ public class Path {
         return resource + DEFAULT_FILE;
     }
 
-    private boolean isDirectory(String resource) throws FileNotFoundException {
+    private boolean isDirectory(String resource) {
         File file = new File(BASE_PATH + resource);
         return file.isDirectory();
     }
--- src/main/java/webserver/ExtensionType.java
+++ src/main/java/webserver/ExtensionType.java
@@ -19,10 +19,10 @@ public enum ExtensionType {
         this.contentType = contentType;
     }
 
-    /*
-     파일의 URL이 주어지면 파일의 확장자 타입 이름에 맞게 Content Type을 반환해줍니다
-     fileUrl은 이미 최종적으로 파싱된 주소가 넘어오기 때문에, 따로 디렉토리일 경우를 체크해주지 않습니다
-     찾는 확장자가 없을 경우에는 정상적으로 파일을 읽을 수 없는 상황이기 때문에, 404 에러 페이지로 응답하기 위한 text/html를 넘겨줍니다.
+    /**
+     *      파일의 URL이 주어지면 파일의 확장자 타입 이름에 맞게 Content Type을 반환해줍니다
+     *      fileUrl은 이미 최종적으로 파싱된 주소가 넘어오기 때문에, 따로 디렉토리일 경우를 체크해주지 않습니다
+     *      찾는 확장자가 없을 경우에는 정상적으로 파일을 읽을 수 없는 상황이기 때문에, 404 에러 페이지로 응답하기 위한 text/html를 넘겨줍니다.
      */
     public static String getContentType(String fileUrl) {
         String fileName = new File(fileUrl).getName();

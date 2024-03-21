d847218266d0804d9de025645661a777285f0dae
seondays <agseon9@gmail.com>
2024-03-21 11:24:56 +0900

    refactor : Request에서 body를 읽어와서 Map으로 저장하도록 수정
    기존에는 String으로 body를 저장 후에 다른 곳에서 변경 후 사용하였는데, create와 login 모두 Json값으로 body가 들어오기 때문에 아예 처음에 세팅할때부터 Map으로 하도록 코드 수정

--- src/main/java/webserver/RequestParser.java
+++ src/main/java/webserver/RequestParser.java
@@ -1,10 +1,16 @@
 package webserver;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 import utils.HttpMethods;
 
 public class RequestParser {
     public static final String REQUEST_DELIMITER = " ";
     public static final String QUERY_DELIMITER = "\\?";
+    public static final String JSON_TEXT_MARK = "\"";
+    public static final String JSON_ELEMENTS_DELIMITER = ",";
+    public static final String JSON_KEY_VALUE_DELIMITER = ":";
     private final String[] requestPath;
 
     public RequestParser(String requestLine) {
@@ -21,6 +27,18 @@ public class RequestParser {
             return queryPieces[1];
         }
         return "";
+    }
+
+    public Map<String, String> parseJsonToMap(String info) {
+        // 중괄호와 콜론 제거
+        info = info.substring(1, info.length() - 1).replaceAll(JSON_TEXT_MARK, "");
+        // , 으로 분리 후 다시 : 로 분리
+        return Arrays.stream(info.split(JSON_ELEMENTS_DELIMITER))
+                .map(pieces -> pieces.split(JSON_KEY_VALUE_DELIMITER))
+                .collect(Collectors.toMap(
+                        key -> key[0],
+                        value -> value[1]
+                ));
     }
 
     public String getUserResource() {

a3b43b4130622378648b3874c1ae03a9659405ff
seondays <agseon9@gmail.com>
2024-03-23 22:41:45 +0900

    refactor : 리다이렉션 주소를 관리 부분 삭제
    리다이렉션 주소는 각 핸들러에서 처리하도록 수정함에 따라 코드 삭제

--- src/main/java/utils/Path.java
+++ src/main/java/utils/Path.java
@@ -8,10 +8,8 @@ public class Path {
     public static final String DEFAULT_FILE = "/index.html";
 
     /**
-     *      요청받은 리소스 정보를 가지고 유효한 주소를 만들어준다.
-     *      요청 리소스가 디렉토리라면 해당 디렉토리의 index.html 파일의 주소를,
-     *      파일이라면 해당 파일의 주소를 생성한 후에, 해당 주소가 유효한지 확인한다.
-     *      유효하지 않은 파일일 경우 예외를 발생시킨다.
+     * 요청받은 리소스 정보를 가지고 유효한 주소를 만들어준다. 요청 리소스가 디렉토리라면 해당 디렉토리의 index.html 파일의 주소를, 파일이라면 해당 파일의 주소를 생성한 후에, 해당 주소가 유효한지
+     * 확인한다. 유효하지 않은 파일일 경우 예외를 발생시킨다.
      */
     public String buildURL(String resource) throws FileNotFoundException {
         StringBuffer result = new StringBuffer(BASE_PATH);
@@ -27,11 +25,6 @@ public class Path {
             throw new FileNotFoundException();
         }
         return result.toString();
-    }
-
-    // 리다이렉션할 주소(파일에 관한 주소가 아니다.) 서버에 보내는 요청 주소 즉 http://localhost:8080/login/index.html 를 리턴해준다
-    public String buildForRedirection(String resource) {
-        return resource + DEFAULT_FILE;
     }
 
     private boolean isDirectory(String resource) {

24fa9d6ddec0e12090003810a0c74d387eb0962d
seondays <agseon9@gmail.com>
2024-03-27 11:22:02 +0900

    feat : 유저 List를 동적으로 제공하기 위한 기능 구현
    로그인 된 상태에서 유저 List를 조회하고자 하는 요청이 올 때, 현재 유저 List를 파일에 추가해서 제공하는 기능

--- src/main/java/webserver/handler/DynamicHttpBodyHandler.java
+++ src/main/java/webserver/handler/DynamicHttpBodyHandler.java
@@ -1,24 +1,25 @@
 package webserver.handler;
 
+import db.Database;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import model.User;
 
-public class DynamicHttpHandler {
+public class DynamicHttpBodyHandler {
     public static final String USER_NAME_VIEW = "<p class=\"header__username\">안녕하세요 %s 님</p>";
     public static final String INSERT_INDEX = "<a href=\"/main\">";
 
     /**
      * body로 사용할 파일에 사용자 이름을 넣어 변경해준다.
-     * todo : 구조 변경과 함께 해당 부분 userName을 가져와서 처리하도록 변경 필요
      * todo : 따로 플래그를 심어서 그 부분에서 체크하는 것으로 변경해보기?
      * @param fileUrl
      * @return
      * @throws IOException
      */
-    public byte[] readBodyAddUserName(String fileUrl, String name) throws IOException {
+    public byte[] readAddNameBody(String fileUrl, String name) throws IOException {
         File file = new File(fileUrl);
         FileInputStream fileInputStream = new FileInputStream(file);
         BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream));
@@ -28,6 +29,26 @@ public class DynamicHttpHandler {
             sb.append(line).append("\n");
             if (line.contains(INSERT_INDEX)){
                 sb.append(String.format((USER_NAME_VIEW),name)).append("\n");
+            }
+        }
+        return sb.toString().getBytes();
+    }
+
+    public byte[] readUserListBody(String fileUrl) throws IOException {
+        File file = new File(fileUrl);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream));
+        StringBuffer sb = new StringBuffer();
+        String line;
+        while((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+            if (line.contains("<tbody>")){
+                for(User u: Database.findAll()) {
+                    sb.append("<tr>").append("\n");
+                    sb.append(String.format("<td>%s</td>", u.getUserId())).append("\n");
+                    sb.append(String.format("<td>%s</td>", u.getName())).append("\n");
+                    sb.append("</tr>").append("\n");
+                }
             }
         }
         return sb.toString().getBytes();

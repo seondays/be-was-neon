af0131c656d52e4c26196527bd8a2b906a4f0b0e
seondays <agseon9@gmail.com>
2024-04-01 00:00:16 +0900

    feat : request 요청의 body를 저장할 객체 구현
    request 요청에서 body 타입의 객체 따로 구현.
    body에서 사용자 정보를 얻기 위한 get 메서드들이 구현되어 있다.

---
+++ src/main/java/webserver/httpElement/HttpRequestBody.java
@@ -0,0 +1,34 @@
+package webserver.httpElement;
+
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpRequestBody {
+    private Logger logger = LoggerFactory.getLogger(HttpRequestBody.class);
+    private Map<String, String> body;
+
+    public HttpRequestBody(Map<String, String> body) {
+        this.body = body;
+    }
+
+    public Map<String, String> getBody() {
+        return body;
+    }
+
+    public String getUserId() {
+        return body.get("userId");
+    }
+
+    public String getPassword() {
+        return body.get("password");
+    }
+
+    public String getName() {
+        return body.get("name");
+    }
+
+    public String getEmail() {
+        return body.get("email");
+    }
+}
